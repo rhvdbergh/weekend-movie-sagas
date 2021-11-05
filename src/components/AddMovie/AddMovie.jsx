c11b5beb3e0624efe5edae1adb58583f233d4d67
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-05 16:32:43 -0500

    basic navbar, addmovie components

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,5 @@
+function AddMovie() {
+  return <p>On Add Movie</p>;
+}
+
+export default AddMovie;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,14 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import MovieDetails from '../MovieDetails/MovieDetails';
+import AddMovie from '../AddMovie/AddMovie';
+import Navbar from '../Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
+      <Navbar />
       <Router>
         <Route path="/" exact>
           <MovieList />
@@ -18,6 +21,8 @@ function App() {
         </Route>
 
         {/* Add Movie page */}
+        <Route path="/add"></Route>
+        <AddMovie />
       </Router>
     </div>
   );
