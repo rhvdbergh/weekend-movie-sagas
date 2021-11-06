b1c754bd1219029d8e1c64232cbb345f8771219d
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-05 20:37:28 -0500

    initial mui setup

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // Create the rootSaga generator function
 function* rootSaga() {
