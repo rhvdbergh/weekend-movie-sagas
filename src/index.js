fe8250e293e150bce6542c3b65754cee60a2e320
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-07 11:29:45 -0600

    adds comments to index.js after redux files feature

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './components/App/App.js';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
+// import the reducers (where the sagas are imported)
 import storeInstance from './modules/reducers.js';
-
+// import the fonts for mui
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
