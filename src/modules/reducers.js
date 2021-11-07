25c5b9a207d0adace57ca0d5650029adbefcc8e6
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-07 11:27:37 -0600

    extracts sagas and reducers to their own modules

---
+++ src/modules/reducers.js
@@ -0,0 +1,55 @@
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import rootSaga from './sagas.js';
+
+// Used to store movies returned from the server
+const movies = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_MOVIES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+// Used to store the movie genres
+const genres = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_GENRES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+// Used to store the movie for which details are being viewed
+const selectedMovie = (state = { genres: [] }, action) => {
+  switch (action.type) {
+    case 'SET_SELECTED_MOVIE':
+      return action.payload;
+    case 'RESET_SELECTED_MOVIE':
+      return {};
+    default:
+      return state;
+  }
+};
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Create one store that all components can use
+const storeInstance = createStore(
+  combineReducers({
+    movies,
+    genres,
+    selectedMovie,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+export default storeInstance;
